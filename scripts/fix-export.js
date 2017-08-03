68a450c183095c7327e600da2bbd37ff7e26109a
Tim Scanlin <tscanlin@gmail.com>
2017-08-03 00:10:31 -0700

    fix export fix

--- scripts/fix-export.js
+++ scripts/fix-export.js
@@ -4,13 +4,14 @@ const config = require('../next.config.js')
 
 const fs = require('fs-extra')
 
-globby(`./out${config.assetPrefix}_next/*/page`).then((files) => {
+const prefix = config.assetPrefix
+
+globby(`out/**/page`).then((files) => {
   const folder = files[0]
-  const newFolder = folder.split('/page').join(config.assetPrefix)
-  console.log(files)
+  const newFolder = folder.split('/page').join(prefix)
   console.log('Copying: ', folder, newFolder);
   fs.copySync(folder, newFolder)
-  fs.moveSync(newFolder, folder)
+  fs.moveSync(newFolder, folder + prefix)
   return files
 }).catch((e) => {
   console.log(e)

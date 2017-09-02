6ea5594308abdc2100bdd3f0b4a19a11e371b01c
Tim Scanlin <tscanlin@gmail.com>
2017-09-01 19:02:24 -0700

    work on screenshot diffing

--- test/integration_test.js
+++ test/integration_test.js
@@ -4,6 +4,7 @@ Feature('Index');
 Scenario('test page load', (I) => {
   I.amOnPage('/')
   I.see('Tocbot')
-  I.seeElement('.is-active-link')
+  I.seeElement('.toc-link')
+  // I.wait(1)
   I.saveScreenshot('tocbot.png', true)
 });

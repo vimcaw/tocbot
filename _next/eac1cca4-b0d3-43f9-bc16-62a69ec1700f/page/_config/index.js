2eac48d577575fd14a0fa7ddaf512bffbd25a687
Travis CI <nobody@nobody.org>
2018-02-03 19:22:03 +0000

    Deploy: log event in demo page

---
+++ _next/eac1cca4-b0d3-43f9-bc16-62a69ec1700f/page/_config/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/_config', function() {
+            var comp = module.exports=webpackJsonp([6],{350:function(t,e,s){t.exports=s(351)},351:function(t,e,s){"use strict";Object.defineProperty(e,"__esModule",{value:!0});var o=s(174),n=function(t){return t&&t.__esModule?t:{default:t}}(o);e.default={title:"Tocbot",subtitle:"Generate a table of contents based on the heading structure of an html document",description:"Tocbot - Generate a table of contents based on the heading structure of an html document",stylesheets:["https://unpkg.com/tachyons@4.7.0/css/tachyons.min.css",n.default.assetPrefix+"/static/css/tocbot.css",n.default.assetPrefix+"/static/css/styles.css"],topLinks:[{text:"About",href:n.default.assetPrefix+"/"},{text:"Changelog",href:n.default.assetPrefix+"/changelog"},{text:"Github",href:"https://github.com/tscanlin/tocbot"}],user:"tscanlin",repo:"tocbot",siteId:"UA-76620957-1"}}},[350]);
+            return { page: comp.default }
+          })
+        
